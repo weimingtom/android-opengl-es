dd27ab054c96c526a63ad11ed5fdbd48df5adfd1
LuckXudong@gmail.com <LuckXudong@gmail.com@8a536e91-25fb-986e-5ff8-2403b349a0de>
2012-02-03 01:52:46 +0000

    绕某个点旋转？完成

--- trunk/src/ice/animation/RotateAnimation.java
+++ trunk/src/ice/animation/RotateAnimation.java
@@ -19,11 +19,28 @@ public class RotateAnimation extends Animation {
         this.rotateZ = rotateZ;
     }
 
+    public void setCenterOffset(float translateX, float translateY, float translateZ) {
+        this.translateX = translateX;
+        this.translateY = translateY;
+        this.translateZ = translateZ;
+    }
+
     @Override
     protected void onAttach(GL11 gl, float interpolatedTime) {
+
+        boolean offset = translateX != 0 || translateY != 0 || translateZ != 0;
+
+        if (offset)
+            gl.glTranslatef(translateX, translateY, translateZ);
+
         float angle = fromAngle + ((toAngle - fromAngle) * interpolatedTime);
         gl.glRotatef(angle, rotateX, rotateY, rotateZ);
+
+        if (offset)
+            gl.glTranslatef(-translateX, -translateY, -translateZ);
     }
+
+    private float translateX, translateY, translateZ;
 
     private float fromAngle, toAngle;
     private float rotateX, rotateY, rotateZ;
