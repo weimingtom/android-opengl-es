a8cc1ccf78d3272edd27b99d35958d173d93d6d6
LuckXudong <LuckXudong@gmail.com>
2012-03-01 06:24:38 +0000

    AtlasSequence （OK）

--- src/ice/node/widget/AtlasSequence.java
+++ src/ice/node/widget/AtlasSequence.java
@@ -28,47 +28,47 @@ public class AtlasSequence extends OverlayParent {
 
         float mostRight = totalWidth / 2 - eachWidth / 2;
 
-
-        if (size == 0) {
-
-            List<AtlasOverlay> overlays = new ArrayList<AtlasOverlay>(sequence.length);
-
-            for (int i = 0; i < sequence.length; i++) {
-
-                AtlasOverlay atlasOverlay = new AtlasOverlay(eachWidth, eachHeight, splitU, splitV);
-
-                atlasOverlay.setAtlasIndex(sequence[i], atlasTexture);
-
-                atlasOverlay.setPos(mostRight - i * (eachWidth / 2 + margin), 0);
-
-                overlays.add(atlasOverlay);
-            }
-
-            addChildren(overlays);
-        }
-        else if (size == sequence.length) {
-            for (int i = 0; i < sequence.length; i++) {
+        if (size < sequence.length || size == 0) {
+            for (int i = 0; i < size; i++) {
                 AtlasOverlay atlasOverlay = (AtlasOverlay) get(i);
                 atlasOverlay.setAtlasIndex(sequence[i], atlasTexture);
+                atlasOverlay.setPos(mostRight - i * (eachWidth / 2 + margin), 0);
                 atlasOverlay.setVisible(true);
             }
+
+            List<AtlasOverlay> more = new ArrayList<AtlasOverlay>(sequence.length);
+
+            for (int i = 0; i < sequence.length - size; i++) {
+                AtlasOverlay atlasOverlay = new AtlasOverlay(eachWidth, eachHeight, splitU, splitV);
+                atlasOverlay.setAtlasIndex(sequence[i], atlasTexture);
+                atlasOverlay.setPos(mostRight - i * (eachWidth / 2 + margin), 0);
+
+                more.add(atlasOverlay);
+            }
+
+            addChildren(more);
         }
         else {
+            for (int i = 0; i < size; i++) {
 
-        }
-        this.sequence = sequence;
-    }
+                AtlasOverlay atlasOverlay = (AtlasOverlay) get(i);
 
-    public void resetSequence() {
-        for (int i = 0; i < size(); i++) {
-            get(i).setVisible(false);
+                if (i < sequence.length) {
+                    atlasOverlay.setPos(mostRight - i * (eachWidth / 2 + margin), 0);
+                    atlasOverlay.setAtlasIndex(sequence[i], atlasTexture);
+                    atlasOverlay.setVisible(true);
+                }
+                else {
+                    atlasOverlay.setVisible(false);
+                }
+
+            }
         }
+
     }
 
     private Texture atlasTexture;
     private int splitU, splitV;
-
-    private int[] sequence;
 
     private int eachWidth, eachHeight;
     private float margin;
